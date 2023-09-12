c1831a2553b61d9eeec7d883a732ac25d0a858fb
Pavan Mantha <manthapavankumar11@gmail.com>
2023-09-12 21:16:41 +0530

    added conversational capability and faiss storage capabilities

--- core/SummarizerAndTranslator.py
+++ core/SummarizerAndTranslator.py
@@ -1,16 +1,50 @@
 import openai
 import os
 import streamlit as st
+import faiss
+
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chains import RetrievalQAWithSourcesChain
 
 
 def transcribe_audio(file_path):
-    openai.api_key = os.getenv("api_key")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     if file_path is not None:
         with open(file_path, 'rb') as audio_file:
             transcript = openai.Audio.transcribe("whisper-1", audio_file)
             if transcript is not None:
-                st.write(transcript)
+                # st.write(transcript)
                 try:
                     os.remove(file_path)
                 except Exception as e:
                     print(e.__cause__)
+                return transcript
+
+
+def qa_retrieval(prompt, transcript):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    # Splitting the text
+    textsplitter = CharacterTextSplitter(chunk_size=512, chunk_overlap=0)
+
+    texts = textsplitter.split_text(transcript.text)
+
+    store = FAISS.from_texts(
+        texts, OpenAIEmbeddings(), metadatas=[{"source": f"Text chunk {i} of {len(texts)}"} for i in range(len(texts))]
+    )
+
+    faiss.write_index(store.index, "docs.faiss")
+
+    llm = ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo")
+
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=store.as_retriever()
+    )
+
+    answer = chain({"question": prompt}, return_only_outputs=True)
+
+    return answer["answer"]
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from core.FileUploadHandler import handle_upload_files
-from core.SummarizerAndTranslator import transcribe_audio
+from core.SummarizerAndTranslator import transcribe_audio, qa_retrieval
 import os
 
 st.title("🦜 Langchain + OpenAI - Audio Transcription and Summarization")
@@ -9,18 +9,62 @@ with st.sidebar:
     st.title('🦜 OpenAI API Key')
     if 'api_key' in st.secrets and len(st.secrets['api_key']) > 0 and str(st.secrets['api_key']).startswith('sk-'):
         st.success('API key already provided!', icon='✅')
-        openai_api_api = st.secrets['api_key']
+        openai_api_key = st.secrets['api_key']
     else:
-        openai_api_api = st.text_input('Enter OpenAI API Key:', type='password')
-        if not (openai_api_api.startswith('sk-') and len(openai_api_api) > 0):
+        openai_api_key = st.text_input('Enter OpenAI API Key:', type='password')
+        if not (openai_api_key.startswith('sk-') and len(openai_api_key) > 0):
             st.warning('Please enter your openai key!', icon='⚠️')
         else:
             st.success('Proceed to uploading your audio file!', icon='✅')
-    os.environ['api_key'] = openai_api_api
+        os.environ['OPENAI_API_KEY'] = openai_api_key
 
-uploaded_file = st.file_uploader("Choose a [.mp3, .mp4, .wav, .webm] file", accept_multiple_files=False)
+uploaded_file = st.sidebar.file_uploader("Choose a [.mp3, .mp4, .wav, .webm] file", accept_multiple_files=False)
 file = handle_upload_files(uploaded_file)
-transcribe_audio(file_path=file)
+transcript = transcribe_audio(file_path=file)
+if transcript is not None:
+    st.sidebar.success("Audio transcribed successfully, please ask your questions now")
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+
+# Function for generating OpenAI response
+def question_and_answer_routine(prompt_input):
+    return qa_retrieval(prompt=prompt_input, transcript=transcript)
+
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not openai_api_key):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = question_and_answer_routine(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
 
 
 
